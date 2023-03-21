d0e8231128163b99a9c668e4104d39439f395f0d
ellen-woodward <s00222321@atu.ie>
2023-03-21 16:02:50 +0000

    not working

--- src/app/services/car-api.service.ts
+++ src/app/services/car-api.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { catchError, Observable, tap } from 'rxjs';
+import { ICar } from '../interfaces/cars';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarApiService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+
+  private _siteURL = "http://localhost:5050/cars"
+
+  getCarDetails():Observable<ICar> {
+
+    return this._http.get<ICar>(this._siteURL)
+    .pipe(
+      tap(data => console.log('car data/error' + JSON.stringify(data))
+    ),
+    catchError(this.handleError)
+    );
+  }
+
+
+  addCarDetails(car:ICar):Observable<any> {
+   return this._http.post<ICar>(this._siteURL, car)
+   .pipe(
+    tap(data => console.log('add car message/error' + JSON.stringify(data))
+    ),
+    catchError(this.handleError)
+    );
+   }
+
+
+  delCarDetails(carId:string):Observable<any> {
+    let deleteURL=this._siteURL+":"+carId;
+    return this._http.delete(deleteURL)
+    .pipe(
+      tap(data => console.log('del car message/error' + JSON.stringify(data))
+    ),
+    catchError(this.handleError)
+    );
+ 
+  }
+
+  private handleError (err:HttpErrorResponse) {
+    console.log('CarApiService: ' + err.message);
+    return Observable.throw(err.message);
+  }
 }
